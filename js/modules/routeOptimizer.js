186e9bcd9ed53c3cee7bfb5bac4ecf1ad3e64203
Kunwar Bansal <kunwarbansal@users.noreply.github.com>
2026-09-21 19:40:21 +0530

    fix(map): switch to OpenStreetMap official tiles with high-contrast dark theme

--- js/modules/routeOptimizer.js
+++ js/modules/routeOptimizer.js
@@ -135,11 +135,10 @@ export class RouteOptimizerModule {
       zoomControl: true
     }).setView([28.0, 77.0], 7);
 
-    // Dark-themed tiles without watermark
-    L.tileLayer("https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png", {
-      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> &copy; <a href="https://carto.com/">CARTO</a>',
-      maxZoom: 19,
-      subdomains: 'abcd'
+    // 100% Free OpenStreetMap standard tiles (Zero API key, Zero watermark)
+    L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      maxZoom: 19
     }).addTo(this.map);
   }
 
