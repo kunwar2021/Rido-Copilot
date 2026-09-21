3a3e6347ad3c81254c42e01781a83e32a1743033
Kunwar Bansal <kunwarbansal@users.noreply.github.com>
2026-09-21 19:38:23 +0530

    fix(map): update basemap tile provider to eliminate watermark

--- js/modules/routeOptimizer.js
+++ js/modules/routeOptimizer.js
@@ -135,10 +135,11 @@ export class RouteOptimizerModule {
       zoomControl: true
     }).setView([28.0, 77.0], 7);
 
-    // Dark-themed tiles
-    L.tileLayer("https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png", {
-      attribution: '&copy; <a href="https://carto.com/">CARTO</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>',
-      maxZoom: 18
+    // Dark-themed tiles without watermark
+    L.tileLayer("https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png", {
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> &copy; <a href="https://carto.com/">CARTO</a>',
+      maxZoom: 19,
+      subdomains: 'abcd'
     }).addTo(this.map);
   }
 
